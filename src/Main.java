667c66951269467a4a8920498b19cebd5e31ae95
Mikhail Starilov <starilov.m@gmail.com>
2020-11-25 16:35:03 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPrice = 14025_10;
+        long bonusMiles = ticketPrice /100 / 20;
+
+        System.out.println(bonusMiles);
+    }
+}
