4cacfc0550450e257706de80db167b23a21f825b
Mikhail Starilov <starilov.m@gmail.com>
2020-11-25 17:04:54 +0600

    New variable added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         long ticketPrice = 14025_10;
-        long bonusMiles = ticketPrice /100 / 20;
+        int everyRuble = 20;
+        long bonusMiles = ticketPrice /100 / everyRuble;
 
         System.out.println(bonusMiles);
     }
